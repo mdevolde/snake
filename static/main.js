de516307cf01a62148b733d121980e5f3b24da1d
= <martin.devolder2@gmail.com>
2024-08-24 16:31:16 +0200

    - Patching a bug (game started with any key pressed) - Adding sprites for snake and apple

--- static/main.js
+++ static/main.js
@@ -1,3 +1,5 @@
+// Snake spritesheet: https://rembound.com/files/creating-a-snake-game-tutorial-with-html5/snake-graphics.png
+
 import init, { 
     new_game, move_snake, update_game,
     get_map_width, get_map_height, get_up_key, get_down_key, get_left_key, get_right_key
@@ -57,15 +59,16 @@ async function run() {
     }
 
     $(document).keydown(function(e){
-        if (game_started == false) {
-            game_started = true;
-            if (!gameOverElement.classList.contains('hidden')) {
-                gameOverElement.classList.add('hidden');
+        if (e.keyCode == UP_KEY || e.keyCode == DOWN_KEY || e.keyCode == LEFT_KEY || e.keyCode == RIGHT_KEY) {
+            if (game_started == true) {
+                game = move_snake(e.keyCode);
+            } else {
+                game_started = true;
+                if (!gameOverElement.classList.contains('hidden')) {
+                    gameOverElement.classList.add('hidden');
+                }
+                gameLoop(0);
             }
-            gameLoop(0);
-        } else if (game_started == true && (e.keyCode == UP_KEY || e.keyCode == DOWN_KEY 
-            || e.keyCode == LEFT_KEY || e.keyCode == RIGHT_KEY)) {
-            game = move_snake(e.keyCode);
         }
     });    
 
@@ -88,26 +91,105 @@ async function run() {
 
     function drawSnake() {
         let snake = game.snake.body;
-        snake.forEach(drawSnakePart)
-    }
-
-    function drawSnakePart(snakePart) {
-        snakeboard_ctx.fillStyle = "green";
-        snakeboard_ctx.strokestyle = "darkgreen";
-        snakeboard_ctx.fillRect(snakePart.x * DisplayFactor, snakePart.y * DisplayFactor, DisplayFactor, DisplayFactor);
-        snakeboard_ctx.strokeRect(snakePart.x * DisplayFactor, snakePart.y * DisplayFactor, DisplayFactor, DisplayFactor);
+        snake.forEach((segment, index) => {
+            let segx = segment.x;
+            let segy = segment.y;
+            let tilex = segx * DisplayFactor;
+            let tiley = segy * DisplayFactor;
+    
+            // Init positions on the sprite sheet
+            let tx = 0;
+            let ty = 0;
+    
+            // Determine correct image for the head
+            if (index === 0) {
+                let nseg = snake[index + 1]; // Next segment
+                if (segy < nseg.y) {
+                    // Top
+                    tx = 3; ty = 0;
+                } else if (segx > nseg.x) {
+                    // Right
+                    tx = 4; ty = 0;
+                } else if (segy > nseg.y) {
+                    // Bottom
+                    tx = 4; ty = 1;
+                } else if (segx < nseg.x) {
+                    // Left
+                    tx = 3; ty = 1;
+                }
+            // Determine correct image for the tail
+            } else if (index === snake.length - 1) {
+                let pseg = snake[index - 1]; // Previous segment
+                if (pseg.y < segy) {
+                    // Top
+                    tx = 3; ty = 2;
+                } else if (pseg.x > segx) {
+                    // Right
+                    tx = 4; ty = 2;
+                } else if (pseg.y > segy) {
+                    // Bottom
+                    tx = 4; ty = 3;
+                } else if (pseg.x < segx) {
+                    // Left
+                    tx = 3; ty = 3;
+                }
+            // Determine correct image for the body
+            } else {
+                let pseg = snake[index - 1]; // Previous segment
+                let nseg = snake[index + 1]; // Next segment
+                if (pseg.x < segx && nseg.x > segx || nseg.x < segx && pseg.x > segx) {
+                    // Horizontal left-right
+                    tx = 1; ty = 0;
+                } else if (pseg.x < segx && nseg.y > segy || nseg.x < segx && pseg.y > segy) {
+                    // Angle left-bottom
+                    tx = 2; ty = 0;
+                } else if (pseg.y < segy && nseg.y > segy || nseg.y < segy && pseg.y > segy) {
+                    // Vertical top-bottom
+                    tx = 2; ty = 1;
+                } else if (pseg.y < segy && nseg.x < segx || nseg.y < segy && pseg.x < segx) {
+                    // Angle top-left
+                    tx = 2; ty = 2;
+                } else if (pseg.x > segx && nseg.y < segy || nseg.x > segx && pseg.y < segy) {
+                    // Angle right-top
+                    tx = 0; ty = 1;
+                } else if (pseg.y > segy && nseg.x > segx || nseg.y > segy && pseg.x > segx) {
+                    // Angle bottom-right
+                    tx = 0; ty = 0;
+                }
+            }
+    
+            snakeboard_ctx.drawImage(
+                tileimage,
+                tx * 64, ty * 64, // Coordinates in the sprite
+                64, 64, // Size of the cut
+                tilex, tiley, // Position on the canvas
+                DisplayFactor, DisplayFactor // Size on the canvas
+            );
+        });
     }
 
     function drawFood() {
         let food = game.food;
-        snakeboard_ctx.fillStyle = "red";
-        snakeboard_ctx.strokestyle = "darkred";
-        snakeboard_ctx.fillRect(food.x * DisplayFactor, food.y * DisplayFactor, DisplayFactor, DisplayFactor);
-        snakeboard_ctx.strokeRect(food.x * DisplayFactor, food.y * DisplayFactor, DisplayFactor, DisplayFactor);
+    
+        const appleX = 0 * 64; // X position of the apple in the sprite
+        const appleY = 3 * 64; // Y position of the apple in the sprite
+    
+        snakeboard_ctx.drawImage(
+            tileimage, 
+            appleX, appleY, // Coordinates in the sprite
+            64, 64, // Size of the cut
+            food.x * DisplayFactor, food.y * DisplayFactor, // Position on the canvas
+            DisplayFactor, DisplayFactor // Size on the canvas
+        );
     }
 
     function displayScore() {
         scoreElement.textContent = game.score;
     }
 }
-run();
+
+let tileimage = new Image();
+tileimage.src = '/static/img/snake-sprites.png';
+tileimage.onload = function() {
+    run();
+};
